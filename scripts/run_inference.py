64508bee3842639a977be84a3146dfc1f92b30a2
Tanaya Atmaram Kambli <tatmaram@crusoe.ai>
2026-08-11 12:44:58 -0700

    corrected dataset repo

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -27,7 +27,7 @@ print("Loading CommonVoice samples...")
 start = time.time() 
 
 dataset = load_dataset(
-    "mozilla-foundation/common_voice_11_0",
+    "echodict/common_voice_11_0",
     "en",
     split="test",
     cache_dir=DATA_CACHE,
